caf1b6932a3f8ab491d29ba9dad685a01c4bb9a1
joao jacinto <joaonetotfm@gmail.com>
2023-04-08 20:04:13 -0300

    feature: visuable bubble sort

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const form = document.getElementById("form");
 const value = document.getElementById("value");
-var bars = [];
 const TOTAL_VALUE = 400;
+var values = [100, 40, 60, 80, 75, 25, 10, 30];
+var clicks = 0;
 
 function removeAllChilds() {
   document.getElementById("data-container").innerHTML = "";
 }
 
+function bubbleSort() {
+  var aux;
+  for (let i = 0; i < clicks; i++) {
+    swaped = 0;
+    for (let j = 0; j < clicks - 1 - i; j++) {
+      if (values[j] > values[j + 1]) {
+        aux = values[j];
+        values[j] = values[j + 1];
+        values[j + 1] = aux;
+      }
+    }
+  }
+}
+
 function updateGraphic() {
   removeAllChilds();
-  console.log(bars);
-  bars.forEach((bar) => {
+  values.forEach((bar) => {
     let b = document.createElement("div");
-    let height = (bar.value * 100) / TOTAL_VALUE;
+    let height = (bar * 100) / TOTAL_VALUE;
     if (height > 100) height = 100;
     b.style.height = height + "%";
     b.classList.add("bar");
     document.getElementById("data-container").appendChild(b);
   });
+  bubbleSort();
 }
 
 const formHandler = (event) => {
+  clicks++;
   event.preventDefault();
-  const data = event.target.data.value;
-  const bar = { value: data };
-  bars.push(bar);
   updateGraphic();
   form.reset();
 };

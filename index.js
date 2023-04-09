01ce471a42828c926080f2dc44b71e599773c786
joao jacinto <joaonetotfm@gmail.com>
2023-04-09 11:07:33 -0300

    feature: implements selection sort

--- index.js
+++ index.js
@@ -1,53 +1,72 @@
 const bubbleSortForm = document.getElementById("bubble-sort-form");
 const selectionSortForm = document.getElementById("selection-sort-form");
 const TOTAL_VALUE = 400;
-var values = [100, 40, 60, 80, 75, 25, 10, 30];
+var globalValues = [100, 40, 60, 80, 75, 25, 10, 30];
+var values = {
+  bubblesort: [...globalValues],
+  selectionsort: [...globalValues],
+};
+
 var clicks = 0;
 
 function removeAllChilds(algoritm) {
-  console.log(algoritm)
-  document.getElementById("bubble-sort").innerHTML = "";
+  let element = document.getElementById(algoritm + "-content");
+  element.innerHTML = "";
+  console.clear();
 }
 
 function bubbleSort() {
   var aux;
   for (let i = 0; i < clicks; i++) {
-    swaped = 0;
     for (let j = 0; j < clicks - 1 - i; j++) {
-      if (values[j] > values[j + 1]) {
-        aux = values[j];
-        values[j] = values[j + 1];
-        values[j + 1] = aux;
+      if (values.bubblesort[j] > values.bubblesort[j + 1]) {
+        aux = values.bubblesort[j];
+        values.bubblesort[j] = values.bubblesort[j + 1];
+        values.bubblesort[j + 1] = aux;
       }
     }
   }
 }
 
+function selectionSort() {
+  var aux;
+  var minorIndex = 0;
+  for (let i = 0; i < values.selectionsort.length; i++) {
+    for (let j = 0; j < values.selectionsort.length; j++) {
+      if (values.selectionsort[i] > values.selectionsort[minorIndex]) {
+        minorIndex = i;
+      }
+    }
+    aux = values.selectionsort[i];
+    values.selectionsort[i] = values.selectionsort[minorIndex];
+    values.selectionsort[minorIndex] = aux;
+  }
+}
+
 function updateGraphic(algoritm) {
   removeAllChilds(algoritm);
-  values.forEach((bar) => {
+  values[algoritm.replace("-", "")].forEach((bar) => {
     let b = document.createElement("div");
     let height = (bar * 100) / TOTAL_VALUE;
     if (height > 100) height = 100;
     b.style.height = height + "%";
     b.classList.add("bar");
-    document.getElementById("bubble-sort").appendChild(b);
+    console.log(algoritm);
+    document.getElementById(algoritm + "-content").appendChild(b);
   });
-  bubbleSort();
+  if (algoritm == "bubble-sort") {
+    bubbleSort();
+  }
+  if (algoritm == "selection-sort") {
+    selectionSort();
+  }
 }
 
 const formHandler = (event) => {
   let algoritm = event.target.name;
   clicks++;
   event.preventDefault();
-  switch (algoritm) {
-    case "bubbleSort":
-      updateGraphic("bubble-sort");
-    case "selectionSort":
-      updateGraphic("selection-sort");
-    default:
-      updateGraphic("bubble-sort");
-  }
+  updateGraphic(algoritm);
   updateGraphic();
   bubbleSortForm.reset();
   selectionSortForm.reset();
